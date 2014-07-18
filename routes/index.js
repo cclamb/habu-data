adffc4b26d214660eb3a9f41500722985f212b9b
Chris <cclamb@ece.unm.edu>
2014-07-18 07:26:55 -0600

    trying to serve static artifacts

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/index', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/', function(req, res) {
+  res.sendfile('index.html');
 });
 
 module.exports = router;
