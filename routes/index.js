41fe2e4f2397258db076513ab4ea9d02705361fd
Chris <cclamb@ece.unm.edu>
2014-07-23 08:00:58 -0600

    created image test file, added hooks for image manipulation

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ router.get('/index', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/image', function(req, res) {
+  res.sendfile('public/image.html');
+});
+
 router.get('/style', function(req, res) {
   res.sendfile('public/stylesheets/alternative-style.css');
 });
